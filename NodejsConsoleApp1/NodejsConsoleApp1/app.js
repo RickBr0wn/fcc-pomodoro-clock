45c449d5e764083109d576db7a5faab22dc028f8
RickBr0wn <ricky.brown.00@gmail.com>
2018-04-08 18:11:49 +0100

    Updating UI - work in progress

--- NodejsConsoleApp1/NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/NodejsConsoleApp1/app.js
@@ -1,8 +1,29 @@
 'use strict';
 
-let time = 59;
+$("#timer-length").knob({
+    'min': 1,
+    'max': 60,
+    'thickness': 0.06,
+    'height': 100,
+    'width': 100,
+    'fgColor': '#fff',
+    'bgColor': 'rgba(0, 0, 0, 0.1)'
+});
 
-setInterval(function () {
-    document.getElementById('timer').innerHTML = time;
-    time--;
-}, 1000);
+$("#break-length").knob({
+    'min': 1,
+    'max': 60,
+    'thickness': 0.06,
+    'height': 100,
+    'width': 100,
+    'fgColor': '#fff',
+    'bgColor': 'rgba(0, 0, 0, 0.1)'
+});
+
+$("#timer").knob({
+    'min': 1,
+    'max': 60,
+    'thickness': 0.06,
+    'fgColor': '#fff',
+    'bgColor': 'rgba(0, 0, 0, 0.1)'
+});
