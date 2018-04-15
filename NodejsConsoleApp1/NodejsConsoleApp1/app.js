30cd26b03daafdd6b678e271c5a51fc45e3a0367
RiCK BROWN <ricky.brown.00@gmail.com>
2018-04-15 02:50:54 +0100

    Added timer buttons

--- NodejsConsoleApp1/NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/NodejsConsoleApp1/app.js
@@ -4,8 +4,8 @@ $("#timer-length").knob({
     'min': 1,
     'max': 60,
     'thickness': 0.06,
-    'height': 100,
-    'width': 100,
+    'height': 60,
+    'width': 60,
     'fgColor': '#fff',
     'bgColor': 'rgba(0, 0, 0, 0.1)'
 });
@@ -14,8 +14,8 @@ $("#break-length").knob({
     'min': 1,
     'max': 60,
     'thickness': 0.06,
-    'height': 100,
-    'width': 100,
+    'height': 60,
+    'width': 60,
     'fgColor': '#fff',
     'bgColor': 'rgba(0, 0, 0, 0.1)'
 });
@@ -27,3 +27,27 @@ $("#timer").knob({
     'fgColor': '#fff',
     'bgColor': 'rgba(0, 0, 0, 0.1)'
 });
+
+function timeMinusClick(){
+    let tMinus = $("#timer-length").val();
+    tMinus--;
+    $('#timer-length').val(tMinus).trigger('change');
+}
+
+function timePlusClick(){
+    let tPlus = $("#timer-length").val();
+    tPlus++;
+    $('#timer-length').val(tPlus).trigger('change');
+}
+
+function breakMinusClick(){
+    let bMinus = $("#break-length").val();
+    bMinus--;
+    $('#break-length').val(bMinus).trigger('change');
+}
+
+function breakPlusClick(){
+    let bPlus = $("#break-length").val();
+    bPlus++;
+    $('#break-length').val(bPlus).trigger('change');
+}
